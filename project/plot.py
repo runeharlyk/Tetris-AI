a9da39358283c7a41f04f1705d67110166a9334b
Rune Harlyk <rune@harlyk.dk>
2023-12-20 18:21:36 +0100

    Adds iteractive scatterplot

---
+++ project/plot.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from time import sleep
+
+class ScatterPlot:
+    margin = 0.05
+    def __init__(self, xlabel, ylabel, title) -> None:
+        plt.ion()
+        self.fig, self.ax = plt.subplots()
+        self.x, self.y = [],[]
+        self.sc = self.ax.scatter(self.x, self.y)
+        self.ax.set_xlabel(xlabel)
+        self.ax.set_ylabel(ylabel)
+        self.ax.set_title(title)
+        plt.draw()
+
+    def add_point(self, x, y):
+        self.x.append(x)
+        self.y.append(y)
+        ax = plt.gca()
+        xmin, xmax = min(self.x), max(self.x)
+        ymin, ymax = min(self.y), max(self.y)
+        dx, dy = (xmax - xmin) * self.margin, (ymax - ymin) * self.margin
+        ax.set_xlim([xmin - dx, xmax + dx])
+        ax.set_ylim([ymin - dy, ymax + dy])
+        self.sc.set_offsets(np.c_[self.x, self.y])
+        self.fig.canvas.draw_idle()
+        plt.pause(0.001)
+
+if __name__ == '__main__':
+    plot = ScatterPlot("Test", "X-axis", "Y-axis")
+    for i in range(1000):
+        plot.add_point(i, i)
+        sleep(0.01)
