bfee9bac07a8d2fe9251e57fd9fcdf686867323a
Rune Harlyk <rune@harlyk.dk>
2024-01-06 16:07:00 +0100

    Adds test helper function with theory decorator

---
+++ test/helper.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+path = "test/board_configurations/"
+
+def load_board(id):
+    return np.loadtxt(path + f'board{id}.txt')
+
+def parametrized(dec):
+    def layer(*args, **kwargs):
+        def repl(f):
+            return dec(f, *args, **kwargs)
+        return repl
+    return layer
+
+@parametrized
+def theory(test, theories):
+    def aux(*args):
+        return [test(*args, *theory) for theory in theories]
+    return aux
