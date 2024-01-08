12ed7dd90f7baaf428421edf5756b4b6f9418ef6
Lucas Mortensen <lucas37c@gmail.com>
2024-01-08 22:08:56 +0100

    Adds elitism and other small syntax changes

--- train_GeneticAgent.py
+++ train_GeneticAgent.py
@@ -1,25 +1,29 @@
 import random
-from environment.tetris import Tetris
 import numpy as np
+from environment.tetris import Tetris
 
 class GeneticAgent():
     
-    def __init__(self, state_size,rows=20,cols=10, population_size=20) -> None:
+    def __init__(self, state_size, elite, rows=20,cols=10, population_size=40) -> None:
         self.state_size = state_size
         self.rows = rows
         self.cols = cols
+        self.elite = elite
         
         self.weights = [[random.uniform(-1,1) for _ in range(4)] for _ in range(population_size)]
     
     def mutate(self,weights):
-        return [w + random.uniform(-0.03,0.03) for w in weights]        
+        return [w + random.uniform(-0.02,0.02) for w in weights]        
+    
+    def act(self, states, weights):
+        return max(states.items(), key=lambda x: (sum(x[1][i] * weights[i] for i in range(0, 4))))[0]       
         
     def get_fitness(self,weights,max_steps = 1000):
         self.env = Tetris(self.rows,self.cols)
         game_over = False
         
         steps = 0
-        while game_over == False:
+        while not game_over:
             next_states = self.env.get_possible_states()
             best_action = self.act(next_states,weights)
             done, score, _ = self.env.step(*best_action)
@@ -32,7 +36,8 @@ class GeneticAgent():
         return score        
     
     def breed(self,parent1,parent2):
-        child = [(p1 + p2) / 2 for p1, p2 in zip(parent1, parent2)]
+        split = random.randint(1, len(parent1) - 1)
+        child = parent1[:split] + parent2[split:]
         return self.mutate(child)
     
     def generation(self, weights, steps):
@@ -50,7 +55,7 @@ class GeneticAgent():
         
         return best_results
        
-    def play(self, num_parents=10, num_gens = 10):
+    def play(self, num_parents=20, num_gens = 10):
         population = self.weights
         
         for gen in range(num_gens):
@@ -60,26 +65,19 @@ class GeneticAgent():
             scores = [score for _, score in weight_score]
             
             print(f'Generation: {gen + 1} | High score: {np.max(scores)} | Mean score: {np.mean(scores)}')
-            
+            elite = [weights for weights, _ in weight_score[:self.elite]]
             parents = [weights for weights, _ in weight_score[:num_parents]]
             
-            next_gen = []
-            for _ in range(len(population)):
+            next_gen = elite[:]
+            for _ in range(len(population)-len(elite)):
                 parent1, parent2 = random.sample(parents, 2)
                 child = self.breed(parent1, parent2)
                 next_gen.append(child)
                 
             population = next_gen
-            
-            
         
-        
-    def act(self, states, weights):
-        return max(states.items(), key=lambda x: (sum(x[1][i] * weights[i] for i in range(0, 4))))[0]       
     
 if __name__ == "__main__":
-    agent = GeneticAgent(5)
+    agent = GeneticAgent(state_size=5,elite=3)
     agent.play(100)
-
-        
         
