1cb9505d9a1e0037043a4b9b2088059bbb5ee8a2
Rune Daugaard Harlyk <rdh@edora.hq>
2024-01-09 10:55:14 +0100

    Moves env to training from model

--- agents/GeneticAgent.py
+++ agents/GeneticAgent.py
@@ -1,41 +1,36 @@
-import random
-from environment.tetris import Tetris
+import numpy as np
 
 
 class GeneticAgent():
-    
-    def __init__(self, state_size, elite, rows=20,cols=10, population_size=40) -> None:
+    def __init__(self, state_size, elite, population_size=40) -> None:
         self.state_size = state_size
-        self.rows = rows
-        self.cols = cols
         self.elite = elite
         
-        self.weights = [[random.uniform(-1,1) for _ in range(4)] for _ in range(population_size)]
+        self.weights = [[np.random.uniform(-1, 1) for _ in range(4)] for _ in range(population_size)]
     
     def mutate(self,weights):
-        return [w + random.uniform(-0.02,0.02) for w in weights]        
+        return [w + np.random.uniform(-0.02, 0.02) for w in weights]        
     
     def act(self, states, weights):
         return max(states.items(), key=lambda x: (sum(x[1][i] * weights[i] for i in range(0, 4))))[0]       
         
-    def get_fitness(self,weights,max_steps = 1000):
-        self.env = Tetris(self.cols,self.rows)
-        game_over = False
-        
+    def get_fitness(self, env, weights, max_steps = 1000):
+        env.reset()
         steps = 0
-        while not game_over:
-            next_states = self.env.get_possible_states()
+        done = False
+        while not done:
+            next_states = env.get_possible_states()
             best_action = self.act(next_states,weights)
-            done, score, _ = self.env.step(*best_action)
+            done, score, _ = env.step(*best_action)
             
             steps += 1
             
-            if done or steps > max_steps:
-                game_over = True
+            if steps > max_steps:
+                break
             
         return score        
     
     def breed(self,parent1,parent2):
-        split = random.randint(1, len(parent1) - 1)
+        split = np.random.randint(1, len(parent1) - 1)
         child = parent1[:split] + parent2[split:]
         return self.mutate(child)
