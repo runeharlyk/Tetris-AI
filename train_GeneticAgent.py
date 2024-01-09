c50f6e5c395b02f5081ef0707214057ba12921cf
Rune Daugaard Harlyk <rdh@edora.hq>
2024-01-09 10:55:14 +0100

    Moves env to training from model

--- train_GeneticAgent.py
+++ train_GeneticAgent.py
@@ -3,20 +3,18 @@ import numpy as np
 from environment.tetris import Tetris
 from agents.GeneticAgent import GeneticAgent
 
-agent = GeneticAgent(state_size=5,elite=3)
+agent = GeneticAgent(state_size=5, elite=3)
+env = Tetris(10, 20)
        
-    
 def play(agent, num_parents=20, num_gens = 10):
     population = agent.weights
     
-    
     for gen in range(num_gens):
         
-        weight_score = [(weights, agent.get_fitness(weights)) for weights in population]
+        weight_score = [(weights, agent.get_fitness(env, weights)) for weights in population]
         weight_score.sort(key=lambda x: x[1], reverse=True)
         
         scores = [score for _, score in weight_score]
-        
         
         print(f'Generation: {gen + 1} | High score: {np.max(scores)} | Mean score: {np.mean(scores)}')
         elite = [weights for weights, _ in weight_score[:agent.elite]]
