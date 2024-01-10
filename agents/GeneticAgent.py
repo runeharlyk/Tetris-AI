94c0e4b3f5c7ec04e35633095fe47b2e9f99fd6e
Lucas Mortensen <lucas37c@gmail.com>
2024-01-10 12:26:32 +0100

    Adds variability to breeding and mutation

--- agents/GeneticAgent.py
+++ agents/GeneticAgent.py
@@ -2,20 +2,27 @@ import numpy as np
 
 
 class GeneticAgent():
-    def __init__(self, state_size, elite, population_size, max_steps, num_parents, num_gens) -> None:
+    def __init__(self, state_size, elite, population_size, max_steps, num_parents, num_gens, mutation_value, mutation_chance) -> None:
         self.state_size = state_size
         self.elite = elite
         self.max_steps = max_steps
         self.population_size = population_size
         self.num_parents = num_parents
         self.num_gens = num_gens
+        self.mutation_value = mutation_value
+        self.mutation_chance = mutation_chance
         
         self.weights = np.random.uniform(-1, 1, (population_size, state_size))
+        
     def mutate(self,weights):
-        return [w + np.random.uniform(-0.02, 0.02) for w in weights]        
+        for i in range(len(weights)):
+            if np.random.random() < self.mutation_chance:
+                weights[i] += np.random.uniform(-self.mutation_value, self.mutation_value)       
+        return weights
+          
     
     def act(self, states, weights):
-        return max(states.items(), key=lambda x: (sum(x[1][i] * weights[i] for i in range(0, 4))))[0]       
+        return max(states.items(), key=lambda x: (sum(x[1][i] * weight for i, weight in enumerate(weights))))[0]       
         
     def get_fitness(self, env, weights):
         env.reset()
@@ -34,6 +41,8 @@ class GeneticAgent():
         return score        
     
     def breed(self,parent1,parent2):
-        split = np.random.randint(1, len(parent1) - 1)
-        child = np.concatenate([parent1[:split], parent2[split:]]) 
-        return self.mutate(child)
+        split = np.random.randint(1, len(parent1))
+        child1 = np.concatenate([parent1[:split], parent2[split:]])
+        child2 = np.concatenate([parent2[:split], parent1[split:]])
+        
+        return child1,child2
