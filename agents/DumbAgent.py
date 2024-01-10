85c749b265d1a29a3202aa29b202ce60188142e1
Lucas Mortensen <lucas37c@gmail.com>
2024-01-10 16:35:12 +0100

    Updates DumbAgent and adds new weights from Genetic

--- agents/DumbAgent.py
+++ agents/DumbAgent.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class DumbAgent():
     def __init__(self, state_size):
-        self.weights = np.array([0.064, -0.31, -0.29, -0.89])
+        self.weights = np.array([0.27803437,-0.62152025,-0.15910121,-0.94336524,0.36147187])
 
     def act(self, states):
         return max(states.items(), key=lambda x: (sum(x[1][i] * self.weights[i] for i in range(0, 3))))[0]
