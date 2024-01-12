39d4fa73345761b798afe29843564a050f993be4
Rune Harlyk <rune@harlyk.dk>
2024-01-12 21:18:00 +0100

    Adds parallel progressbar

--- train_GeneticAgent.py
+++ train_GeneticAgent.py
@@ -26,35 +26,27 @@ agent = GeneticAgent(state_size, elite_pct, population_size, max_steps, num_gens
 
 
 def train():
+    envs = [Tetris(cols, rows, level_multi) for _ in range(population_size)]
     plot = ScatterPlot("Games", "Score", "Score per game | training")
     all_scores = np.zeros((num_gens, population_size))
+    best_per_gen = np.zeros((num_gens, state_size))
     tqdm.write(f'|==== # of generations: {num_gens}\t| Max steps: {max_steps}\t| Pop. size: {population_size}\t| Elite %: {elite_pct} ====|')
-    num_processes = multiprocessing.cpu_count()
-    for gen in tqdm(range(agent.num_gens)):
-        args = [(Tetris(cols, rows, level_multi), weight) for weight in agent.weights]
-        try:
-            with multiprocessing.Pool(processes = num_processes) as pool:
-                scores = np.array(pool.starmap(agent.get_fitness, args))
-        except Exception as error:
-            print(error)
-        finally:
-            pool.close()
-            pool.join()
-            pass
+    for gen in tqdm(range(agent.num_gens), desc='Generations'):
+        args = [(envs[i], weight) for i, weight in enumerate(agent.weights)]
+        with multiprocessing.Pool(processes = multiprocessing.cpu_count()) as pool:
+            scores = np.array(list(tqdm(pool.imap_unordered(agent.get_fitness, args), desc='Population done', leave=False)))
 
         agent.weights = agent.weights[scores.argsort()[::-1]]
         
         all_scores[gen] = scores
         survivors = int(np.ceil(elite_pct*population_size))
-        
-        tqdm.write(f'| Min./Mean/Max./Std. [Gen {gen+1}]: {np.min(scores)} / {np.round(np.mean(scores),2)} / {np.max(scores)} / {np.std(scores)}\t| Min./Mean/Max./Std. [Overall]: {np.min(all_scores[:gen+1])} / {np.round(np.mean(all_scores[:gen+1]),2)} / {np.max(all_scores[:gen+1])} /  {np.std(all_scores)}\t|')
-        
         parents = agent.weights[:int(population_size*parent_pct)-survivors]
         
         for i in range(survivors, population_size, 2):
             idx = np.random.randint(0, len(parents), size=2)
             parent1, parent2 = parents[idx,:]
             
+        best_per_gen[gen] = agent.weights[scores.argmax()]
             if np.random.random() < crossover_rate:
                 child1, child2 = agent.breed(parent1, parent2)
                 
@@ -67,7 +59,7 @@ def train():
         for i, score in enumerate(scores):
             plot.add_point(i+gen*population_size,score, True)
     tqdm.write(f'Final best weights: {agent.weights[0]}')  
-    plot.save('scores.csv')
+    plot.save('genetic_scores.csv')
 
 if __name__ == '__main__': 
     train()
