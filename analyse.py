ae3584cad44bc49e22aa87dbd84e860cb6237ba4
Rune Harlyk <rune@harlyk.dk>
2024-01-13 12:25:32 +0100

    Adds histogram of lines cleared per step

--- analyse.py
+++ analyse.py
@@ -1,16 +1,25 @@
 import argparse
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
 
-
-parser = argparse.ArgumentParser(prog='Analyse results', description='Analyse and compare models')
-parser.add_argument('--path', default="results/genetic_1705104088")
+parser = argparse.ArgumentParser(
+    prog="Analyse results", description="Analyse and compare models"
+)
+parser.add_argument("--path", default="results/genetic_1705104088")
 args = parser.parse_args()
 
-def analyse(path = None):
-    model_path = path or args.path
-    scores = np.loadtxt(f'{model_path}/scores.txt')
-    line_history = np.loadtxt(f'{model_path}/line_history.txt')
 
+def analyse(path=None):
+    model_path = path or args.path
+    scores = np.loadtxt(f"{model_path}/scores.txt")
+    line_history = np.loadtxt(f"{model_path}/line_history.txt")
+    print_descriptive(scores, line_history)
+    show_cleared_lined_per_step(line_history)
+
+
+def print_descriptive(scores, line_history):
     print(f"n samples:\t{len(scores)}")
     print(f"Mean:\t{np.mean(scores)}")
     print(f"Std:\t{np.std(scores)}")
@@ -19,6 +28,19 @@ def analyse(path = None):
     print(f"Max:\t{np.max(scores)}")
     avg_reward = np.sum(scores) / np.sum(np.sum(line_history))
     print(f"Avg score per move:\t{avg_reward}")
+
+
+def show_cleared_lined_per_step(line_history):
+    sns.set(style="darkgrid")
+    df = pd.DataFrame(line_history)
+    column_sums = df.sum()
+    sns.barplot(x=column_sums.index, y=column_sums.values)
+    plt.title("Histogram over lines cleared per step")
+    plt.xlabel("Lines cleared")
+    plt.ylabel("Number of steps")
+    plt.xticks(range(line_history.shape[1]))
+    plt.show()
+
 
 if __name__ == "__main__":
     analyse()
