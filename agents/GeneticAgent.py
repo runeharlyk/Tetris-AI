5296b49cdddd8ec571c107a2d4143935ac2717f5
Rune Harlyk <rune@harlyk.dk>
2024-01-14 20:41:54 +0100

    Adds new genetic methods

--- agents/GeneticAgent.py
+++ agents/GeneticAgent.py
@@ -1,28 +1,56 @@
 import numpy as np
 
 
-class GeneticAgent():
-    def __init__(self, state_size, elite, population_size, max_steps, num_gens, mutation_value, mutation_chance) -> None:
+class GeneticAgent:
+    def __init__(
+        self,
+        state_size,
+        elite,
+        population_size,
+        max_steps,
+        num_gens,
+        mutation_value,
+        mutation_rate,
+    ) -> None:
         self.state_size = state_size
         self.elite = elite
         self.max_steps = max_steps
         self.population_size = population_size
         self.num_gens = num_gens
         self.mutation_value = mutation_value
-        self.mutation_chance = mutation_chance
-        
+        self.mutation_rate = mutation_rate
+
         self.weights = np.random.uniform(-1, 1, (population_size, state_size))
-        
-    def mutate(self,weights):
-        for i in range(len(weights)):
-            if np.random.random() < self.mutation_chance:
-                weights[i] += np.random.uniform(-self.mutation_value, self.mutation_value)       
-        return weights
-          
-    
+
+    def mutate(self, individual):
+        for i in range(len(individual)):
+            if np.random.random() < self.mutation_rate:
+                individual[i] += np.random.normal(0, 0.1)
+        return individual
+
+    def select_parents(self, population, fitness_scores, tournament_size=3):
+        parents = []
+        for _ in range(2):  # Select two parents
+            tournament = np.random.choice(len(population), tournament_size)
+            best_individual = tournament[
+                np.argmax(np.array(fitness_scores)[tournament])
+            ]
+            parents.append(population[best_individual])
+        return parents
+
+    def crossover(self, parent1, parent2):
+        crossover_point = np.random.randint(1, len(parent1))
+        offspring1 = np.concatenate(
+            [parent1[:crossover_point], parent2[crossover_point:]]
+        )
+        offspring2 = np.concatenate(
+            [parent2[:crossover_point], parent1[crossover_point:]]
+        )
+        return offspring1, offspring2
+
     def act(self, states, weights):
-        return max(states.items(), key=lambda x: (sum(x[1][i] * weight for i, weight in enumerate(weights))))[0]       
-        
+        return max(states.items(), key=lambda x: (np.dot(x[1], weights)))[0]
+
     def get_fitness(self, args):
         env, weights = args
         env.reset()
@@ -32,17 +60,17 @@ class GeneticAgent():
             next_states = env.get_possible_states()
             best_action = self.act(next_states, weights)
             done, score, _ = env.step(*best_action)
-            
+
             steps += 1
-            
+
             if steps > self.max_steps:
                 break
-            
-        return score        
-    
+
+        return score
+
     def breed(self, parent1, parent2):
         crossover = np.random.randint(1, len(parent1))
         child1 = np.concatenate([parent1[:crossover], parent2[crossover:]])
         child2 = np.concatenate([parent2[:crossover], parent1[crossover:]])
-        
+
         return child1, child2
--- train_GeneticAgent.py
+++ train_GeneticAgent.py
@@ -3,64 +3,103 @@ import numpy as np
 import multiprocessing
 from environment.tetris import Tetris
 from agents.GeneticAgent import GeneticAgent
+
 multiprocessing.freeze_support()
 from utils.plot import ScatterPlot
 
-level_multi     = False
-cols            = 10
-rows            = 20
-state_size      = 5
+level_multi = False
+cols = 10
+rows = 20
+state_size = 5
 
 population_size = 20
-elite_pct       = 0.1
-parent_pct      = 0.8
+elite_pct = 0.1
+parent_pct = 0.8
 
-num_gens        = 10
-max_steps       = 1000
+num_generations = 10
+max_steps = 1000
 
-mutation_value  = 0.02
-mutation_chance = 0.9
-crossover_rate  = 0.7
+mutation_value = 0.02
+mutation_rate = 0.05
+crossover_rate = 0.7
 
-agent = GeneticAgent(state_size, elite_pct, population_size, max_steps, num_gens, mutation_value, mutation_chance)
+agent = GeneticAgent(
+    state_size,
+    elite_pct,
+    population_size,
+    max_steps,
+    num_generations,
+    mutation_value,
+    mutation_rate,
+)
 
 
 def train():
     envs = [Tetris(cols, rows, level_multi) for _ in range(population_size)]
     plot = ScatterPlot("Games", "Score", "Score per game | training")
-    all_scores = np.zeros((num_gens, population_size))
-    best_per_gen = np.zeros((num_gens, state_size))
-    tqdm.write(f'|==== # of generations: {num_gens}\t| Max steps: {max_steps}\t| Pop. size: {population_size}\t| Elite %: {elite_pct} ====|')
-    for gen in tqdm(range(agent.num_gens), desc='Generations'):
+    all_scores = np.zeros((num_generations, population_size))
+    best_per_gen = np.zeros((num_generations, state_size))
+    tqdm.write(
+        f"|==== # of generations: {num_generations}\t| Max steps: {max_steps}\t| Pop. size: {population_size}\t| Elite %: {elite_pct} ====|"
+    )
+    for generation in tqdm(range(agent.num_gens), desc="Generations"):
         args = [(envs[i], weight) for i, weight in enumerate(agent.weights)]
-        with multiprocessing.Pool(processes = multiprocessing.cpu_count()) as pool:
-            scores = np.array(list(tqdm(pool.imap_unordered(agent.get_fitness, args), desc='Population done', leave=False)))
+        with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+            fitness_scores = np.array(
+                list(
+                    tqdm(
+                        pool.imap_unordered(agent.get_fitness, args),
+                        desc="Population done",
+                        leave=False,
+                    )
+                )
+            )
 
-        agent.weights = agent.weights[scores.argsort()[::-1]]
-        
-        all_scores[gen] = scores
-        survivors = int(np.ceil(elite_pct*population_size))
-        parents = agent.weights[:int(population_size*parent_pct)-survivors]
-        
-        for i in range(survivors, population_size, 2):
-            idx = np.random.randint(0, len(parents), size=2)
-            parent1, parent2 = parents[idx,:]
-            
-        best_per_gen[gen] = agent.weights[scores.argmax()]
+        avg_fitness = np.mean(fitness_scores)
+        best_fitness = np.max(fitness_scores)
+        print(
+            f"Generation {generation + 1}/{num_generations} - Avg Fitness: {avg_fitness:.2f}, Best Fitness: {best_fitness}"
+        )
+
+        new_population = []
+        while len(new_population) < population_size:
+            parent1, parent2 = agent.select_parents(agent.weights, fitness_scores)
             if np.random.random() < crossover_rate:
-                child1, child2 = agent.breed(parent1, parent2)
-                
-            else:   
-                child1, child2 = parent1, parent2
-            
-            agent.weights[i] = child1
-            if i + 1 < population_size:
-                agent.weights[i+1] = child2
-        for i, score in enumerate(scores):
-            plot.add_point(i+gen*population_size,score, True)
+                offspring1, offspring2 = agent.crossover(parent1, parent2)
+            else:
+                offspring1, offspring2 = parent1, parent2
+            new_population.append(agent.mutate(offspring1))
+            new_population.append(agent.mutate(offspring2))
+
+        agent.weights = np.array(new_population)[:population_size]
+
+        # agent.weights = agent.weights[fitness_scores.argsort()[::-1]]
+
+        # all_scores[generation] = fitness_scores
+        # survivors = int(np.ceil(elite_pct * population_size))
+        # parents = agent.weights[: int(population_size * parent_pct) - survivors]
+
+        # for i in range(survivors, population_size, 2):
+        #     idx = np.random.randint(0, len(parents), size=2)
+        #     parent1, parent2 = parents[idx, :]
+
+        #     best_per_gen[generation] = agent.weights[fitness_scores.argmax()]
+        #     if np.random.random() < crossover_rate:
+        #         child1, child2 = agent.breed(parent1, parent2)
+
+        #     else:
+        #         child1, child2 = parent1, parent2
+
+        #     agent.weights[i] = child1
+        #     if i + 1 < population_size:
+        #         agent.weights[i + 1] = child2
+
+        for i, fitness_scores in enumerate(fitness_scores):
+            plot.add_point(i + generation * population_size, fitness_scores, True)
     tqdm.write(f"Final best weights: {agent.weights[0]}")
     plot.save("genetic_scores.csv")
     np.savetxt("best_weights_per_gen.csv", best_per_gen, delimiter=",")
 
-if __name__ == '__main__': 
+
+if __name__ == "__main__":
     train()
